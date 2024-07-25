afed310b6f5718352cbf44e9b927b25d9c50dea9
earthnuker <earthnuker@gmail.com>
2024-07-25 11:04:05 +0200

    install intel-ocl

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
+
 {
   pkgs,
   inputs,
@@ -60,6 +61,11 @@
     enableAllFirmware = true;
     graphics = {
       enable = true;
+    };
+    opengl = {
+      extraPackages = with pkgs; [
+        intel-ocl
+      ];
     };
   };
 
