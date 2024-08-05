cf20c8254e05c5adbdd1ca4be90c306834c3dc75
earthnuker <earthnuker@gmail.com>
2024-08-05 13:44:51 +0200

    Enable TLP

--- configuration.nix
+++ configuration.nix
@@ -247,6 +247,13 @@
         }
       ];
     };
+    tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
+    };
     pipewire = {
       enable = true;
       alsa.enable = true;
