b839e8e32466dbcf5bd624e57d299d14528d6e22
earthnuker <earthnuker@gmail.com>
2024-08-27 09:16:40 +0200

    Fix typo

--- configuration.nix
+++ configuration.nix
@@ -279,7 +279,7 @@
     tlp = {
       enable = true;
       settings = {
-        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
         STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
       };
     };
