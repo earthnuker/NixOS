d111b68a3b64e970fcb23013fc80ef1c4aca65ad
earthnuker <earthnuker@gmail.com>
2024-08-28 08:45:20 +0200

    Add nnn

--- earthnuker.nix
+++ earthnuker.nix
@@ -49,6 +49,7 @@ in
         nix-inspect
         vim.xxd
         devenv
+        nnn
         inputs.nsearch.packages.${pkgs.system}.default
         (writeShellApplication {
           name = "nixdiff";
