dcc312be38d3cd6c96d04926315ba19c4d3a70ce
earthnuker <earthnuker@gmail.com>
2024-10-05 14:06:01 +0200

    Flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Godwaker system configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
