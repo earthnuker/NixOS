25f6c65de6cc2507d5e4ff89140b0b7d06e8806d
earthnuker <earthnuker@gmail.com>
2024-10-07 20:24:02 +0200

    Add lix binary cache

--- configuration.nix
+++ configuration.nix
@@ -185,9 +185,11 @@
     experimental-features = ["nix-command" "flakes" "repl-flake"];
     substituters = [
       "https://nix-community.cachix.org"
+      "https://cache.lix.systems"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
     ];
   };
   nix.nixPath = ["nixpkgs=${nixpkgs.outPath}"];
