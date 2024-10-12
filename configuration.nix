36eba1f6c042f27220e8527775d8f0c97128628d
earthnuker <earthnuker@gmail.com>
2024-10-12 09:03:02 +0200

    Fix kexec

--- configuration.nix
+++ configuration.nix
@@ -209,6 +209,7 @@
     nix-daemon = {
       environment.TMPDIR = "/var/tmp";
     };
+    "prepare-kexec".wantedBy = ["multi-user.target"];
   };
 
   # List packages installed in system profile. To search, run:
