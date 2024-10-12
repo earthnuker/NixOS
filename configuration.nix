b46a8ff5c676cc8b65f60ad76255ecb113aedc19
earthnuker <earthnuker@gmail.com>
2024-10-12 08:08:21 +0200

    enable greetd

--- configuration.nix
+++ configuration.nix
@@ -244,6 +244,7 @@
     deadnix
     nix-web
     nix-tree
+    greetd.tuigreet
   ];
 
   environment.variables = {
@@ -300,6 +301,15 @@
     tailscale = {
       enable = true;
       useRoutingFeatures = "both";
+    };
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+          user = "greeter";
+        };
+      };
     };
     fwupd.enable = true;
     openssh.enable = true;
