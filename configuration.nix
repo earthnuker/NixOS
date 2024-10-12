2097f916ce6105c72f10b82178a3f752f73d38f6
earthnuker <earthnuker@gmail.com>
2024-10-12 09:42:34 +0200

    Use tuigreet

--- configuration.nix
+++ configuration.nix
@@ -304,12 +304,12 @@
     };
     greetd = {
       enable = true;
-      # settings = {
-      #   default_session = {
-      #     command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
-      #     user = "greeter";
-      #   };
-      # };
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+          user = "greeter";
+        };
+      };
     };
     fwupd.enable = true;
     openssh.enable = true;
