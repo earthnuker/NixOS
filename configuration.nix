519263f62d436947acd82fbc093b81e2fa695ad0
earthnuker <earthnuker@gmail.com>
2024-12-18 19:53:04 +0100

    Add themes

--- configuration.nix
+++ configuration.nix
@@ -280,6 +280,44 @@
     fstrim.enable = true;
     thelounge = {
       enable = true;
+      plugins = with pkgs; [
+        nodePackages.thelounge-theme-ion
+        nodePackages.thelounge-theme-nord
+        nodePackages.thelounge-theme-light
+        nodePackages.thelounge-theme-chord
+        nodePackages.thelounge-theme-abyss
+        nodePackages.thelounge-theme-nologo
+        nodePackages.thelounge-theme-crypto
+        nodePackages.thelounge-theme-common
+        nodePackages.thelounge-theme-amoled
+        nodePackages.thelounge-theme-zenburn
+        nodePackages.thelounge-theme-onedark
+        nodePackages.thelounge-theme-gruvbox
+        nodePackages.thelounge-theme-dracula
+        nodePackages.thelounge-theme-classic
+        nodePackages.thelounge-theme-midnight
+        nodePackages.thelounge-theme-hexified
+        nodePackages.thelounge-theme-bmorning
+        nodePackages.thelounge-theme-bdefault
+        nodePackages.thelounge-theme-solarized
+        nodePackages.thelounge-theme-scoutlink
+        nodePackages.thelounge-theme-mortified
+        nodePackages.thelounge-theme-mininapse
+        nodePackages.thelounge-theme-flat-dark
+        nodePackages.thelounge-theme-flat-blue
+        nodePackages.thelounge-theme-seraphimrp
+        nodePackages.thelounge-theme-discordapp
+        nodePackages.thelounge-theme-purplenight
+        nodePackages.thelounge-theme-new-morning
+        nodePackages.thelounge-theme-neuron-fork
+        nodePackages.thelounge-theme-monokai-console
+        nodePackages.thelounge-theme-dracula-official
+        nodePackages.thelounge-theme-zenburn-monospace
+        nodePackages.thelounge-theme-new-morning-compact
+        nodePackages.thelounge-theme-amoled-sourcecodepro
+        nodePackages.thelounge-theme-zenburn-sourcecodepro
+        nodePackages.thelounge-theme-solarized-fork-monospace
+      ];
       extraConfig = {
         defaults = {
           name = "BJZ";
