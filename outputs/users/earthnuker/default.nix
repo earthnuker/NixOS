102439e5765b43bdb95166fa3fe20e4b25d8240c
earthnuker <earthnuker@gmail.com>
2025-04-17 20:36:52 +0200

    move user packages to separate file

--- outputs/users/earthnuker/default.nix
+++ outputs/users/earthnuker/default.nix
@@ -14,59 +14,7 @@
   home = {
     username = "earthnuker";
     homeDirectory = "/home/earthnuker";
-    packages = with pkgs; [
-      alejandra
-      batmon
-      btop
-      dex
-      eza
-      dconf
-      ffmpeg_7-full
-      firefox
-      iftop
-      iotop
-      micromamba
-      fastfetch
-      pandoc
-      rustup
-      cargo-binstall
-      cargo-update
-      zellij
-      tmux
-      vscode
-      i3lock-fancy
-      eww
-      neovide
-      tdesktop
-      xss-lock
-      python3Full
-      starship
-      rofi
-      pavucontrol
-      qemu
-      networkmanager_dmenu
-      nixpacks
-      nix-inspect
-      vim.xxd
-      nnn
-      just
-      mitmproxy
-      bettercap
-      sd
-      rsync
-      grc
-      bat
-      inputs.nsearch.packages.${pkgs.system}.default
-      (writeShellApplication {
-        name = "nixdiff";
-        runtimeInputs = [nvd nix-diff];
-        text = ''
-          set -euxo pipefail
-          nix-diff --word-oriented --skip-already-compared "$1" "$2"
-          nvd diff "$1" "$2"
-        '';
-      })
-    ];
+    packages = (import ./packages.nix) {inherit inputs pkgs;};
 
     sessionVariables = {
       EDITOR = "nvim";
---
+++ outputs/users/earthnuker/packages.nix
@@ -0,0 +1,58 @@
+{
+  inputs,
+  pkgs,
+}:
+with pkgs; [
+  alejandra
+  batmon
+  btop
+  dex
+  eza
+  dconf
+  ffmpeg_7-full
+  firefox
+  iftop
+  iotop
+  micromamba
+  fastfetch
+  pandoc
+  rustup
+  cargo-binstall
+  cargo-update
+  zellij
+  tmux
+  vscode
+  i3lock-fancy
+  eww
+  neovide
+  tdesktop
+  xss-lock
+  python3Full
+  starship
+  rofi
+  pavucontrol
+  qemu
+  networkmanager_dmenu
+  nixpacks
+  nix-inspect
+  vim.xxd
+  nnn
+  just
+  mitmproxy
+  bettercap
+  sd
+  rsync
+  grc
+  bat
+  cachix
+  inputs.nsearch.packages.${pkgs.system}.default
+  (writeShellApplication {
+    name = "nixdiff";
+    runtimeInputs = [nvd nix-diff];
+    text = ''
+      set -euxo pipefail
+      nix-diff --word-oriented --skip-already-compared "$1" "$2"
+      nvd diff "$1" "$2"
+    '';
+  })
+]
